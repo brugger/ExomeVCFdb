6fab7f9ff1ad61d43f11b5df5b120fa989af03fa
Kim Brugger <kim.brugger@easih.ac.uk>
2014-07-16 10:02:03 +0100

    removed all the CP plate logic

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,40 +1,21 @@
-drop database VCFdb;
-create database VCFdb;
-use VCFdb;
+drop database ExomeVCFdb;
+create database ExomeVCFdb;
+use ExomeVCFdb;
 
-
-CREATE TABLE plate (
-  pid                 INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name                VARCHAR(20) NOT NULL ,
-
-  KEY name_idx (name)
-);
 
 
 
 CREATE TABLE sample (
 
   sid                 INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name                VARCHAR(40) NOT NULL ,
-
-  KEY name_idx (name)
-);
-
-
-
-CREATE TABLE sample_sequence (
-
-  ssid                INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  sid                 INT NOT NULL,
-  pid                 INT NOT NULL,
   name                VARCHAR(80) NOT NULL,
   total_reads	      INT,
   mapped_reads	      INT,
   duplicate_reads     INT,
   mean_isize	      float,
 
-  KEY sid_idx  (sid),
-  KEY pid_idx  (pid)
+  KEY sid_idx   (sid),
+  KEY name_idx  (name)
 );
 
 
@@ -48,52 +29,22 @@ CREATE TABLE variant (
   comment	      VARCHAR(200),
   annotation	      VARCHAR(500),
 
+
   KEY pos_idx  (chr, pos, ref, alt)
 );
 
-
-CREATE TABLE region (
-
-  rid                 INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  chr                 VARCHAR(8) NOT NULL ,
-  start               INT NOT NULL,
-  end                 INT NOT NULL,
-  name                VARCHAR(100) NOT NULL ,
-  reference	      VARCHAR(100) NOT NULL,
-
-  KEY pos_idx  (chr, start, end),
-  KEY ref_idx (reference),
-  KEY name_idx (name)
-);
-
-CREATE TABLE coverage (
-
-#  cid                 INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  ssid                INT NOT NULL,
-  rid                 INT NOT NULL,
-
-  min		      FLOAT,  
-  mean		      FLOAT,  
-  max		      FLOAT,  
-  lows		      VARCHAR(300),
-  missing	      VARCHAR(300),
-
-  KEY ssid_idx  (ssid),
-  KEY rid_idx  (rid)
-);
-
-
-
 CREATE TABLE sample_variant (
 
-  ssid                INT NOT NULL,
-  vid                 INT NOT NULL,
+  sid                INT NOT NULL,
+  vid                INT NOT NULL,
 
-  depth		      INT,  
-  AAF		      FLOAT,  
-  quality	      FLOAT,  
+  depth		     INT,    
+  AAF		     varchar(200),	  
+  quality	     FLOAT,  
+  allele_count	     INT NOT NULL,
 
-  KEY ssid_idx  (ssid),
+
+  KEY sid_idx  (sid),
   KEY vid_idx  (vid)
 );
 
